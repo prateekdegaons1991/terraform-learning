fc7a9514a72437fd7827b4eff7a841c245db50b5
pratikdegaon <prateekdegaons1991@outlook.com>
2024-07-28 23:31:15 +0530

    add git actions

--- ec2-instance/ec2-instances.tf
+++ ec2-instance/ec2-instances.tf
@@ -1,12 +1,3 @@
-// This Terraform resource block creates multiple EC2 instances.
-// The instances are created based on the environment variable "env".
-// If the environment variable is set to "development", 2 instances are created.
-// If the environment variable is set to any other value, 3 instances are created.
-// The AMI (Amazon Machine Image) used for the instances is the latest Ubuntu image.
-// The instance type is set by the variable "instance_type".
-// The instances are tagged with the name "terraform-datasource-ec2-{index}" and the creation date.
-// The instances are associated with the security groups specified in the variable "security_groups".
-
 resource "aws_instance" "terraform-datasource-ec2" {
   // The AMI (Amazon Machine Image) used for the instances.
   ami = data.aws_ami.ubuntu.image_id
