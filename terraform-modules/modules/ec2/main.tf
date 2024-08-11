5854f9d936a7db32dcf67d6eb5cfabe35e205d51
pratikdegaon <prateekdegaons1991@outlook.com>
2024-08-11 21:17:47 +0530

    update

--- terraform-modules/modules/ec2/main.tf
+++ terraform-modules/modules/ec2/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-07652eda1fbad7432"
-  instance_type = "t2.micro"
+  ami           = var.ami
+  instance_type = var.instance_type
   tags = {
     Name = "Pratik-Module"
   }
--- terraform-modules/teams/A/ec2-instance.tf
+++ terraform-modules/teams/A/ec2-instance.tf
@@ -1,6 +1,7 @@
 module "ec2-instance" {
     source = "git::https://github.com/prateekdegaons1991/terraform-learning.git//terraform-modules/modules/ec2"
-
+    
+    
 }
 
 
